965a4da34422b27e05f7c2d60961b5548b2cf6f7
Dobri <79987000+NehuaV@users.noreply.github.com>
2023-11-10 01:15:34 +0100

    ref i18n layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        bg: {
+          label: "Bulgarian",
+        },
+      },
+      social: {
+        linkedin: "https://www.linkedin.com/in/dobri-trifonov/",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "TypeScript",
+              autogenerate: { directory: "typescript" },
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
